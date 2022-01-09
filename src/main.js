454be71d8d986d6b0b05701826643e43efcadd76
OceanH <1404939185@qq.com>
2022-01-09 21:15:58 +0800

    安装Vuex,基本配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import store from './store/store'
 
 Vue.config.productionTip = false
 // 全局挂载axios
@@ -9,5 +10,6 @@ Vue.prototype.$http = axios
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount('#app')
